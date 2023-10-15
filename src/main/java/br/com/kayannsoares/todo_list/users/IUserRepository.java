30baa8fe06b2b9cb0f8251e7a89747a190f66294
Kayann Soares <kayan.soares1@gmail.com>
2023-10-15 11:55:48 -0300

    Add Project

---
+++ src/main/java/br/com/kayannsoares/todo_list/users/IUserRepository.java
@@ -0,0 +1,9 @@
+package br.com.kayannsoares.todo_list.users;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID> {
+  UserModel findByUsername(String username);
+}
